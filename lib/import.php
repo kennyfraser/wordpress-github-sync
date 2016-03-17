27fa6c95a499a92fee5c31841509ead61455c259
Kenny <kennyfraser@gmail.com>
2016-03-17 15:49:52 -0300

    Importer updates, added blog processors and more hooks

--- lib/import.php
+++ lib/import.php
@@ -98,33 +98,152 @@ class WordPress_GitHub_Sync_Import {
 		$posts = array();
 		$new   = array();
 
+		$import_results = array();
+
 		foreach ( $commit->tree()->blobs() as $blob ) {
-			if ( ! $this->importable_blob( $blob ) ) {
+
+			$import_results[] = $import_result = (object) array();
+			$import_result->path = $blob->path();
+
+			// is this blob importable?
+			if ( !$this->importable_blob($blob) ) {
+				$import_result->importable = false;
 				continue;
 			}
 
-			$posts[] = $post = $this->blob_to_post( $blob );
-
-			if ( $post->is_new() ) {
-				$new[] = $post;
+			// has the blob changed since the last import?
+			if( !$this->blob_changed($blob) ){
+				$import_result->changed = false;
+				continue;
 			}
+
+			// fetch the function that will be used to import the blob
+			// based on blob properties
+			if( $processor = $this->get_blob_processor($blob) ){
+				if( is_callable($processor) ) {
+
+					// call the processor to import the blob
+					$result = call_user_func_array($processor,[$blob,$this]);
+
+					// if the result is a Post object ( WordPress_GitHub_Sync_Post )
+					if( $result && is_a($result,'WordPress_GitHub_Sync_Post')){
+
+						// add the post to the array of posts to be
+						// imported in to the database
+						$posts[] = $post = $result;
+
+						if ( $post->is_new() ) {
+							// keep track of new posts
+							$new[] = $post;
+						}
+
+						$import_result->new = $post->is_new();
+
+					}else{
+						$import_result->result = $result?$result:false;
+					}
+
+				}else{
+					$import_result->processor = "not callable";
+				}
+
+			}else{
+				$import_result->processor = "no processor";
+			}
+
 		}
 
-		$result = $this->app->database()->save_posts( $posts, $commit->author_email() );
+		// TODO: Log/output results
+		// print_r($import_results);
 
-		if ( is_wp_error( $result ) ) {
-			return $result;
-		}
+		if( count($posts) ){
 
-		if ( $new ) {
-			$result = $this->app->export()->new_posts( $new );
+			$result = $this->app->database()->save_posts( $posts, $commit->author_email() );
 
 			if ( is_wp_error( $result ) ) {
 				return $result;
 			}
+
+			if ( $new ) {
+
+				do_action('wpghs_post_new_post_import', $new);
+
+				$result = $this->app->export()->new_posts( $new );
+
+				if ( is_wp_error( $result ) ) {
+					return $result;
+				}
+			}
+
 		}
 
-		return $posts;
+		return __( 'No commitable posts', 'wordpress-github-sync' );;
+	}
+
+	/**
+	 * Get the function that will be used to import the blob, hookable
+	 *
+	 * @param  WordPress_GitHub_Sync_Blob
+	 * @return callable
+	 */
+	protected function get_blob_processor( WordPress_GitHub_Sync_Blob $blob ) {
+		$processor = false;
+
+		// import any plain text .md document in the defaut way
+		if ( $blob->has_frontmatter()
+				&& $blob->mimetype() == "text/plain"
+				&& $blob->file_extension() == "md" ) {
+			$processor = array($this,'blob_to_post');
+		}
+
+		return apply_filters('wpghs_get_blob_processor', $processor, $blob);
+	}
+
+	/**
+	 * Returns true if the path is not importable, hookable
+	 *
+	 * @param  string
+	 * @return boolean
+	 */
+	protected function is_excluded_path( $path ) {
+
+		$excluded = false;
+
+		// Skip the repo's readme.
+		if ( 'readme' === strtolower( substr( $path, 0, 6 ) ) ) {
+			$excluded = true;
+		}
+
+		return apply_filters('wpghs_exclude_paths', $excluded, $path);
+	}
+
+	/**
+	 * Returns true if the mine type is not importable, hookable
+	 *
+	 * @param  string
+	 * @return boolean
+	 */
+	protected function is_excluded_mime_type( $mime_type ) {
+
+		// nothing excuded by default
+		$excluded = false;
+
+		return apply_filters('wpghs_exclude_mime_types', $excluded, $mime_type);
+	}
+
+	/**
+	 * Returns true if the file extension is not importable, hookable
+	 *
+	 * @param  string
+	 * @return boolean
+	 */
+	protected function is_excluded_file_extension( $file_extension ) {
+
+		// nothing excuded by default
+		$excluded = false;
+
+		// TODO: Test this hook
+		return apply_filters('wpghs_exclude_file_extensions', $excluded, $file_extension);
 	}
 
 	/**
@@ -137,21 +256,36 @@ class WordPress_GitHub_Sync_Import {
 	protected function importable_blob( WordPress_GitHub_Sync_Blob $blob ) {
 		global $wpdb;
 
-		// Skip the repo's readme.
-		if ( 'readme' === strtolower( substr( $blob->path(), 0, 6 ) ) ) {
+		$importable = true;
+
+		if( $this->is_excluded_path($blob->path()) ||
+			$this->is_excluded_file_extension($blob->file_extension()) ||
+			$this->is_excluded_mime_type($blob->path()) ){
+			$importable = false;
+		}
+
+		return apply_filters('wpghs_importable_blob', $importable, $blob);
+	}
+
+	/**
+	 * Checks whether the provided blob has changed.
+	 *
+	 * @param WordPress_GitHub_Sync_Blob $blob Blob to validate.
+	 *
+	 * @return bool
+	 */
+	protected function blob_changed( WordPress_GitHub_Sync_Blob $blob ) {
+		global $wpdb;
+
+		$changed = true;
+
+		// If the blob sha already matches a post, and the path accociated with the sha
+		// has not changed then there has been no change
+		if ( $this->app->database()->sha_exists_with_path( $blob->sha(), $blob->path() ) ){
 			return false;
 		}
 
-		// If the blob sha already matches a post, then move on.
-		if ( ! is_wp_error( $this->app->database()->fetch_by_sha( $blob->sha() ) ) ) {
-			return false;
-		}
-
-		if ( ! $blob->has_frontmatter() ) {
-			return false;
-		}
-
-		return true;
+		return apply_filters('wpghs_blob_changed', $blob, $changed);
 	}
 
 	/**
@@ -188,6 +322,10 @@ class WordPress_GitHub_Sync_Import {
 		}
 
 		$meta['_sha'] = $blob->sha();
+		$meta['_wpghs_github_path']  = $blob->path();
+		
+		$meta = apply_filters('wpghs_blob_to_post_meta', $meta, $blob);
+		$args = apply_filters('wpghs_blob_to_post_args', $args, $meta, $blob);
 
 		$post = new WordPress_GitHub_Sync_Post( $args, $this->app->api() );
 		$post->set_meta( $meta );
